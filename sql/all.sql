89cb23f8371b2034d36c17c1e1c472203e9fe4ac
WillHay <will.hay@roosterpunk.com>
2018-01-20 14:19:03 +0000

    create user table

--- sql/all.sql
+++ sql/all.sql
@@ -1,5 +1,12 @@
-create table user (
+create database therapybox_rock_php;
 
+create table user (
+    id mediumint not null auto_increment,
+    username varchar(255),
+    email varchar(255),
+    password_hash varchar(255),
+    date_signed_up datetime,
+    primary key id,
 );
 
 create table task (
