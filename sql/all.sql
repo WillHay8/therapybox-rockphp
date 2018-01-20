5787189b7f44db18bb9dccf39c30efed0fffecfc
WillHay <will.hay@roosterpunk.com>
2018-01-20 14:15:14 +0000

    add task table sql

--- sql/all.sql
+++ sql/all.sql
@@ -3,5 +3,12 @@ create table user (
 );
 
 create table task (
-
+    id mediumint not null auto_increment,
+    user_id mediumint not null,
+    title text,
+    complete boolean,
+    date_created datetime not null,
+    date_completed datetime,
+    primary key id,
+    foreign key user_id references user(id) on delete cascade on update cascade
 );
